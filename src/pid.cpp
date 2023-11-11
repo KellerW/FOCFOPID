e2d9c7b110fc29b69a8bed964b52716c3628e552
Wagner Keller <wgkeller@outlook.com>
2023-11-11 19:34:06 +0000

    Remove ignored files

---
+++ src/pid.cpp
@@ -0,0 +1,36 @@
+#include "pid.h"
+
+void pid::set_p_gain(double p)
+{
+    p_gain = p;
+}
+void pid::set_i_gain(double i)
+{
+    i_gain = i;
+}
+void pid::set_d_gain(double d)
+{
+    d_gain = d;
+}
+double pid::get_p_gain(void)
+{
+    return p_gain;
+}
+double pid::get_i_gain(void)
+{
+    return i_gain;
+}
+double pid::get_d_gain(void)
+{
+    return d_gain;
+}
+double pid::calcPID(const double set, double err) 
+{
+    setPoint = set;
+    error = err;
+    double e = setPoint - error;
+    
+}
+            
+
+
