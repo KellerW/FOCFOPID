7825cf834f275d87e55e1f71ae7d02ea6c870bb6
Wagner Keller <wgkeller@outlook.com>
2023-11-11 22:33:55 +0000

    Adding Licence and update git ignore files

--- build/src/CMakeFiles/modern_library.dir/pid.cpp.o.d
+++ build/src/CMakeFiles/modern_library.dir/pid.cpp.o.d
@@ -1,3 +1,3 @@
 src/CMakeFiles/modern_library.dir/pid.cpp.o: \
  /home/wkeller/gitroot/fo_pid_bldc/src/pid.cpp /usr/include/stdc-predef.h \
- /home/wkeller/gitroot/fo_pid_bldc/src/../include/pid.hpp
+ /home/wkeller/gitroot/fo_pid_bldc/src/pid.hpp
